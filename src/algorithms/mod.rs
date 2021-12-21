c63312e7b75241fa493724b52349b54ca19c0ee5
Ruslan Akbashev <ruslashev@gmail.com>
2021-12-22 00:51:02 +0300

    move algorithms to algorithms/

---
+++ src/algorithms/mod.rs
@@ -0,0 +1,3 @@
+pub mod count_inversions;
+pub mod insertion_sort;
+pub mod merge_sort;
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 
-mod count_inversions;
-mod insertion_sort;
-mod merge_sort;
+mod algorithms;
 mod rand;
 mod utils;
 
