635d0fac15b4f332c6a6d97d67be115a1d32ddb9
Ruslan Akbashev <ruslashev@gmail.com>
2022-01-02 09:16:50 +0300

    peeking and iterators

--- src/data_structures/linked_list.rs
+++ src/data_structures/linked_list.rs
@@ -9,9 +9,31 @@ struct Node<T> {
     next: Link<T>,
 }
 
+pub struct IntoIter<T>(List<T>);
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,
+}
+
 impl<T> List<T> {
     pub fn new() -> Self {
         List { head: None }
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| {
+            &node.elem
+        })
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| {
+            &mut node.elem
+        })
     }
 
     pub fn push(&mut self, elem: T) {
@@ -29,6 +51,18 @@ impl<T> List<T> {
             node.elem
         })
     }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+
+    pub fn iter(&self) -> Iter<T> {
+        Iter { next: self.head.as_deref() }
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<T> {
+        IterMut { next: self.head.as_deref_mut() }
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -38,6 +72,36 @@ impl<T> Drop for List<T> {
         while let Some(mut node) = it {
             it = node.next.take();
         }
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.elem
+        })
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_deref_mut();
+            &mut node.elem
+        })
     }
 }
 
@@ -71,5 +135,33 @@ mod tests {
         assert!(list.pop() == Some(1));
         assert!(list.pop() == Some(2));
         assert!(list.pop() == Some(3));
+    }
+
+    #[test]
+    fn mutate() {
+        let mut list = List::new();
+
+        list.push(3);
+        list.push(2);
+        list.push(1);
+
+        list.peek_mut().map(|value| { *value = 100 });
+
+        println!("{}", list);
+    }
+
+    #[test]
+    fn iter_mut() {
+        let mut list = List::new();
+
+        list.push(3);
+        list.push(2);
+        list.push(1);
+
+        for item in list.iter_mut() {
+            *item *= 2;
+        }
+
+        println!("{}", list);
     }
 }
