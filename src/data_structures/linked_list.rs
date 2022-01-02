55a26e7b8a4781c4d0ad59cda9e29779bcc615f0
Ruslan Akbashev <ruslashev@gmail.com>
2022-01-02 06:34:25 +0300

    generalize list

--- src/data_structures/linked_list.rs
+++ src/data_structures/linked_list.rs
@@ -1,20 +1,20 @@
-pub struct List {
-    head: Link,
+pub struct List<T> {
+    head: Link<T>,
 }
 
-type Link = Option<Box<Node>>;
+type Link<T> = Option<Box<Node<T>>>;
 
-struct Node {
-    elem: i32,
-    next: Link,
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
 }
 
-impl List {
+impl<T> List<T> {
     pub fn new() -> Self {
         List { head: None }
     }
 
-    pub fn push(&mut self, elem: i32) {
+    pub fn push(&mut self, elem: T) {
         let new = Box::new(Node {
             elem,
             next: self.head.take()
@@ -23,7 +23,7 @@ impl List {
         self.head = Some(new);
     }
 
-    pub fn pop(&mut self) -> Option<i32> {
+    pub fn pop(&mut self) -> Option<T> {
         self.head.take().map(|node| {
             self.head = node.next;
             node.elem
@@ -31,7 +31,7 @@ impl List {
     }
 }
 
-impl Drop for List {
+impl<T> Drop for List<T> {
     fn drop(&mut self) {
         let mut it = self.head.take();
 
@@ -41,7 +41,7 @@ impl Drop for List {
     }
 }
 
-impl std::fmt::Display for List {
+impl<T: std::fmt::Display> std::fmt::Display for List<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         let mut it = &self.head;
 
