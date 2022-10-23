b89feb4f87edef63d42fed55fceca1156cdc53ec
Ruslan Akbashev <ruslashev@gmail.com>
2022-10-24 00:26:43 +0300

    add heap and heap sort

---
+++ src/data_structures/heap.rs
@@ -0,0 +1,122 @@
+use std::mem;
+
+#[derive(Debug)]
+struct BorrowingHeap<'d, T>
+where
+    T: Ord,
+{
+    len: usize,
+    data: &'d mut [T],
+}
+
+impl<'d, T> BorrowingHeap<'d, T>
+where
+    T: Ord + Copy,
+{
+    pub fn from_slice<'s: 'd>(slice: &'s mut [T]) -> Self {
+        let slice_len = slice.len();
+
+        let mut heap = Self {
+            len: slice_len,
+            data: slice,
+        };
+
+        for i in (0..=(slice_len / 2)).rev() {
+            heap.sift_down(i as usize);
+        }
+
+        heap
+    }
+
+    fn parent(i: usize) -> usize {
+        i / 2
+    }
+
+    fn left_child(i: usize) -> usize {
+        2 * i
+    }
+
+    fn right_child(i: usize) -> usize {
+        2 * i + 1
+    }
+
+    fn has_more_priority(&self, x: usize, y: usize) -> bool {
+        self.data[x] > self.data[y]
+    }
+
+    pub fn sift_down(&mut self, mut i: usize) {
+        loop {
+            let l = Self::left_child(i);
+            let r = Self::right_child(i);
+
+            let mut high_pr = i;
+
+            if l < self.len && self.has_more_priority(l, i) {
+                high_pr = l;
+            }
+
+            if r < self.len && self.has_more_priority(r, high_pr) {
+                high_pr = r;
+            }
+
+            if high_pr == i {
+                break;
+            }
+
+            self.data.swap(i, high_pr);
+
+            i = high_pr;
+        }
+    }
+
+    fn sift_up(&mut self, mut i: usize) {
+        let mut p = Self::parent(i);
+
+        while i > 0 && self.has_more_priority(i, p) {
+            self.data.swap(i, p);
+            i = p;
+            p = Self::parent(i);
+        }
+    }
+
+    pub fn inc_key_priority(&mut self, i: usize, new: T) {
+        self.data[i] = new;
+        self.sift_up(i);
+    }
+
+    pub fn insert(&mut self, new: T) {
+        self.len += 1;
+        self.inc_key_priority(self.len - 1, new);
+    }
+
+    pub fn remove(&mut self, i: usize) -> T {
+        let elem = self.data[i];
+        self.data.swap(i, self.len - 1);
+        self.len -= 1;
+
+        if self.has_more_priority(i, Self::parent(i)) {
+            self.sift_up(i);
+        } else {
+            self.sift_down(i);
+        }
+
+        elem
+    }
+}
+
+fn heap_sort<T: Ord + Copy + std::fmt::Debug>(a: &mut [T]) {
+    let mut h = BorrowingHeap::from_slice(a);
+
+    for i in (1..h.len).rev() {
+        h.data.swap(0, i);
+
+        h.len -= 1;
+        h.sift_down(0);
+    }
+}
+
+#[cfg(test)]
+#[test]
+fn test_sort() {
+    crate::tests::test_sort(heap_sort);
+}
