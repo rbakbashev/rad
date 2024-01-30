66d8560b1278bc21e14e15980e88a524c69bc874
Ruslan Akbashev <ruslashev@gmail.com>
2024-01-31 03:06:01 +0400

    tests: rearrange `fn`s, remove `pub`s

--- src/tests.rs
+++ src/tests.rs
@@ -2,61 +2,6 @@ use std::collections::HashMap;
 
 use crate::rand::Wyhash64RNG;
 
-pub fn assert_sorted<T: PartialOrd + std::fmt::Display>(a: &[T]) {
-    for i in 1..a.len() {
-        for j in 0..i {
-            assert!(a[j] <= a[i], "a[{}] = {} > a[{}] = {}", j, a[j], i, a[i]);
-        }
-    }
-}
-
-pub fn generate_array_identical<T: Copy>(n: usize, x: T) -> Vec<T> {
-    vec![x; n]
-}
-
-pub fn generate_array_ascending<T: From<u64>>(n: usize) -> Vec<T> {
-    let mut v = Vec::with_capacity(n);
-    for i in 0..n {
-        v.push((i as u64).into());
-    }
-    v
-}
-
-pub fn generate_array_descending<T: From<u64>>(n: usize) -> Vec<T> {
-    let mut v = Vec::with_capacity(n);
-    for i in 0..n {
-        v.push(((n - i) as u64).into());
-    }
-    v
-}
-
-pub fn generate_array_random(n: usize, lower: u64, upper: u64) -> Vec<u64> {
-    let mut v = Vec::with_capacity(n);
-    let mut r = Wyhash64RNG::new();
-
-    for _ in 0..n {
-        v.push(r.gen_in_range(lower..upper));
-    }
-
-    v
-}
-
-pub fn generate_array_permuation<T: From<u64> + Copy>(n: usize) -> Vec<T> {
-    let mut v = generate_array_ascending(n);
-    permute(&mut v);
-    v
-}
-
-pub fn permute<T: Copy>(v: &mut [T]) {
-    let mut r = Wyhash64RNG::new();
-
-    for i in 0..v.len() {
-        let j = r.gen_in_range(0..v.len() as u64) as usize;
-
-        v.swap(i, j);
-    }
-}
-
 pub fn test_sort(f: fn(&mut [u64])) {
     let len = 1000;
     let id = generate_array_identical(len, 1);
@@ -72,6 +17,53 @@ pub fn test_sort(f: fn(&mut [u64])) {
     test_sort_single(f, perm);
 }
 
+fn generate_array_identical<T: Copy>(n: usize, x: T) -> Vec<T> {
+    vec![x; n]
+}
+
+fn generate_array_ascending<T: From<u64>>(n: usize) -> Vec<T> {
+    let mut v = Vec::with_capacity(n);
+    for i in 0..n {
+        v.push((i as u64).into());
+    }
+    v
+}
+
+fn generate_array_descending<T: From<u64>>(n: usize) -> Vec<T> {
+    let mut v = Vec::with_capacity(n);
+    for i in 0..n {
+        v.push(((n - i) as u64).into());
+    }
+    v
+}
+
+fn generate_array_random(n: usize, lower: u64, upper: u64) -> Vec<u64> {
+    let mut v = Vec::with_capacity(n);
+    let mut r = Wyhash64RNG::new();
+
+    for _ in 0..n {
+        v.push(r.gen_in_range(lower..upper));
+    }
+
+    v
+}
+
+fn generate_array_permuation<T: From<u64> + Copy>(n: usize) -> Vec<T> {
+    let mut v = generate_array_ascending(n);
+    permute(&mut v);
+    v
+}
+
+fn permute<T: Copy>(v: &mut [T]) {
+    let mut r = Wyhash64RNG::new();
+
+    for i in 0..v.len() {
+        let j = r.gen_in_range(0..v.len() as u64) as usize;
+
+        v.swap(i, j);
+    }
+}
+
 fn test_sort_single(f: fn(&mut [u64]), mut values: Vec<u64>) {
     let orig = values.clone();
 
@@ -83,6 +75,14 @@ fn test_sort_single(f: fn(&mut [u64]), mut values: Vec<u64>) {
     let new_counts = count_elements(&values);
 
     assert_eq!(old_counts, new_counts);
+}
+
+fn assert_sorted<T: PartialOrd + std::fmt::Display>(a: &[T]) {
+    for i in 1..a.len() {
+        for j in 0..i {
+            assert!(a[j] <= a[i], "a[{}] = {} > a[{}] = {}", j, a[j], i, a[i]);
+        }
+    }
 }
 
 fn count_elements(values: &[u64]) -> HashMap<u64, usize> {
