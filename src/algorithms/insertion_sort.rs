871f694d15c3e81c23b178fcb1977abade2106ff
Ruslan Akbashev <ruslashev@gmail.com>
2024-01-31 03:22:30 +0400

    insertion_sort: rewrite into original and modified versions

--- src/algorithms/insertion_sort.rs
+++ src/algorithms/insertion_sort.rs
@@ -1,19 +1,49 @@
 fn insertion_sort<T: PartialOrd + Copy>(a: &mut [T]) {
-    for i in 1..a.len() {
-        let key = a[i];
-        let mut j = i as isize - 1;
+    for j in 2..=a.len() {
+        let key = a[j - 1];
+        let mut i = j - 1;
 
-        while j >= 0 && a[j as usize] > key {
-            a[j as usize + 1] = a[j as usize];
-            j -= 1;
+        while i >= 1 && a[i - 1] > key {
+            a[i] = a[i - 1];
+            i -= 1;
         }
 
-        a[(j + 1) as usize] = key;
+        a[i] = key;
+    }
+}
+
+// Same as above except uses 0-indexing
+fn insertion_sort_2<T: PartialOrd + Copy>(a: &mut [T]) {
+    'outer: for j in 1..a.len() {
+        let key = a[j];
+        let mut i = j - 1;
+
+        while a[i] > key {
+            a[i + 1] = a[i];
+
+            if i == 0 {
+                a[i] = key;
+                continue 'outer;
+            }
+
+            i -= 1;
+        }
+
+        a[i + 1] = key;
     }
 }
 
 #[cfg(test)]
-#[test]
-fn test() {
-    crate::tests::test_sort(insertion_sort);
+mod tests {
+    use super::*;
+
+    #[test]
+    fn normal() {
+        crate::tests::test_sort(insertion_sort);
+    }
+
+    #[test]
+    fn alternative() {
+        crate::tests::test_sort(insertion_sort_2);
+    }
 }
