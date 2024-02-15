6b2d71f895690da7220b40e797c51d6020d736bb
Ruslan Akbashev <ruslashev@gmail.com>
2024-02-15 16:18:03 +0400

    algorithms/greedy: add fractional knapsack

--- src/algorithms/greedy.rs
+++ src/algorithms/greedy.rs
@@ -22,6 +22,57 @@ pub fn activity_selection(activities: &[Activity]) -> Vec<Activity> {
     answer
 }
 
+#[derive(Clone, Copy)]
+pub struct Item {
+    value: u32,
+    weight: u32,
+}
+
+// casts are u32 -> f64 lesser than original, and back to u32
+#[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
+pub fn fractional_knapsack(items: &[Item], max_weight: u32) -> Vec<Item> {
+    let mut sorted = items.to_vec();
+
+    sorted.sort_by(|a, b| {
+        let fa = f64::from(a.value) / f64::from(a.weight);
+        let fb = f64::from(b.value) / f64::from(b.weight);
+
+        fb.partial_cmp(&fa)
+            .expect("input contains zero-weight item")
+    });
+
+    let items = sorted;
+
+    let mut answer = vec![];
+    let mut total_weight = 0;
+    let mut i = 0;
+
+    while total_weight < max_weight && i < items.len() {
+        let remaining_weight = max_weight - total_weight;
+
+        if items[i].weight <= remaining_weight {
+            answer.push(items[i]);
+            total_weight += items[i].weight;
+        } else {
+            let frac = f64::from(remaining_weight) / f64::from(items[i].weight);
+
+            let frac_value = (f64::from(items[i].value) * frac).round() as u32;
+            let frac_weight = (f64::from(items[i].weight) * frac).round() as u32;
+
+            answer.push(Item {
+                value: frac_value,
+                weight: frac_weight,
+            });
+
+            total_weight += frac_weight;
+        }
+
+        i += 1;
+    }
+
+    answer
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -46,5 +97,28 @@ mod tests {
         let ans = activity_selection(&activities);
 
         assert_eq!(4, ans.len());
+    }
+
+    #[test]
+    fn fractional_knapsack_test() {
+        let items = [
+            Item {
+                value: 60,
+                weight: 10,
+            },
+            Item {
+                value: 100,
+                weight: 20,
+            },
+            Item {
+                value: 120,
+                weight: 30,
+            },
+        ];
+        let max_weight = 50;
+        let ans = fractional_knapsack(&items, max_weight);
+        let total_value = ans.iter().fold(0, |acc, x| acc + x.value);
+
+        assert_eq!(240, total_value);
     }
 }
