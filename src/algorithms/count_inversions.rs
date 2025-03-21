d5bca2d904f7946fc30f1910b73e6dd18550ab9c
Ruslan Akbashev <ruslashev@gmail.com>
2025-03-21 12:58:47 +0400

    algs/count_inv: remove `Default` bound, simplify like merge_sort

--- src/algorithms/count_inversions.rs
+++ src/algorithms/count_inversions.rs
@@ -1,8 +1,8 @@
-pub fn count_inversions<T: Default + Copy + PartialOrd>(a: &mut [T]) -> usize {
+pub fn count_inversions<T: Copy + PartialOrd>(a: &mut [T]) -> usize {
     count_inv_rec(a, 0, a.len() - 1)
 }
 
-fn count_inv_rec<T: Default + Copy + PartialOrd>(a: &mut [T], l: usize, h: usize) -> usize {
+fn count_inv_rec<T: Copy + PartialOrd>(a: &mut [T], l: usize, h: usize) -> usize {
     if l == h {
         return 0;
     }
@@ -11,49 +11,38 @@ fn count_inv_rec<T: Default + Copy + PartialOrd>(a: &mut [T], l: usize, h: usize
     let left = count_inv_rec(a, l, m);
     let right = count_inv_rec(a, m + 1, h);
 
-    left + right + count_inv_in_subranges(a, l, m, h)
+    left + right + count_inv_in_subrange(a, l, m, h)
 }
 
-fn count_inv_in_subranges<T: Default + Copy + PartialOrd>(
-    a: &mut [T],
-    l: usize,
-    m: usize,
-    h: usize,
-) -> usize {
-    let llen = m - l + 1;
-    let rlen = h - m;
-    let mut left = Vec::new();
-    let mut right = Vec::new();
+fn count_inv_in_subrange<T: Copy + PartialOrd>(a: &mut [T], l: usize, m: usize, h: usize) -> usize {
+    let left = a[l..=m].to_vec();
+    let right = a[m + 1..=h].to_vec();
+
     let mut inv = 0;
-
-    left.resize_with(llen, Default::default);
-    right.resize_with(rlen, Default::default);
-
-    left.copy_from_slice(&a[l..=m]);
-    right.copy_from_slice(&a[m + 1..=h]);
 
     let mut i = 0;
     let mut j = 0;
     let mut k = l;
-    while i < llen && j < rlen {
+
+    while i < left.len() && j < right.len() {
         if left[i] <= right[j] {
             a[k] = left[i];
             i += 1;
         } else {
             a[k] = right[j];
             j += 1;
-            inv += llen - i;
+            inv += left.len() - i;
         }
         k += 1;
     }
 
-    while i != llen {
+    while i != left.len() {
         a[k] = left[i];
         i += 1;
         k += 1;
     }
 
-    while j != rlen {
+    while j != right.len() {
         a[k] = right[j];
         j += 1;
         k += 1;
@@ -62,7 +51,6 @@ fn count_inv_in_subranges<T: Default + Copy + PartialOrd>(
     inv
 }
 
-#[cfg(test)]
 #[test]
 fn test() {
     let mut v = vec![2, 3, 8, 6, 1];
